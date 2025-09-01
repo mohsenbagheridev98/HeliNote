e14c3137f71d29ffed61d962e1dbc2f5c76ae402
mohsenbagheri <mohsenbagheridev98@gmail.com>
2025-09-02 00:39:29 +0330

    complete some ui components

--- app/src/main/java/mohsen/helinote/ui/add_edit_note/components/HintUI.kt
+++ app/src/main/java/mohsen/helinote/ui/add_edit_note/components/HintUI.kt
@@ -2,6 +2,7 @@ package mohsen.helinote.ui.add_edit_note.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HintUI(
@@ -31,10 +33,11 @@ fun HintUI(
             textStyle = textStyle,
             modifier = Modifier
                 .fillMaxWidth()
-                .onFocusChanged { onFocusChange(it) }
+                .onFocusChanged { onFocusChange(it)
+                    }
         )
         if (isHintVisible) {
-            Text(text = hint, style = textStyle, color = Color.DarkGray)
+            Text(text = hint, style = textStyle, color = Color.Gray)
         }
     }
 }
